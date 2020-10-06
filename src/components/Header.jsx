1274acb03d814c63a19424765e1d9f0f15f2837d
Alan W <65926023+AlanWargula@users.noreply.github.com>
2020-10-06 13:41:27 -0700

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import FilterNoneIcon from "@material-ui/icons/FilterNone";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        {" "}
+        <FilterNoneIcon /> Notepad
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
